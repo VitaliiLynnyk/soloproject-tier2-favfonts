642743838a05c704861c92a062f7bd3e5074e08a
VitaliiLynnyk <linnik2101@gmail.com>
2020-02-19 16:46:54 +0100

    add toolbar to main & style it

--- src/app/containers/containers.module.ts
+++ src/app/containers/containers.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { MainComponent } from "./main/main.component";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
+import { ToolbarComponent } from "./main/toolbar/toolbar.component";
 
 @NgModule({
   declarations: [
     MainComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ToolbarComponent
   ],
   exports: [
     MainComponent,
---
+++ src/app/containers/main/toolbar/toolbar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar',
+  templateUrl: './toolbar.html',
+  styleUrls: ['./toolbar.scss']
+})
+export class ToolbarComponent {}
